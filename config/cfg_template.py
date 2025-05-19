70dc6581e885013c1659f789cc355d696105511f
Sam Gallop (NBI) <sam.gallop@nbi.ac.uk>
2025-05-19 15:43:50 +0100

    Configuration templates

---
+++ config/cfg_template.py
@@ -0,0 +1,4 @@
+project_code = "reports"
+project_log = "reporting.log"
+graph_width = 1600
+graph_height = 900
---
+++ config/db_template.py
@@ -0,0 +1,6 @@
+hostname = ""
+port = ""
+database = ""
+username = ""
+password = ""
+use_db_server = True
